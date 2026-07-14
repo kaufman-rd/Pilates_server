8d06e9d0da1d4b625702b9751d1177cf88694a20
dansoskin <dansoskin@gmail.com>
2026-07-14 12:24:19 +0300

    changed async mode

--- backend/config.py
+++ backend/config.py
@@ -5,4 +5,5 @@ from flask_socketio import SocketIO
 
 app = Flask(__name__)
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
+# socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
